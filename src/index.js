53eb02d4223dc457a2bbc265704ac6f01a6f7f74
sktanui <shadrackkemei2019@gmail.com>
2024-10-17 11:57:54 +0300

    complete resubmission

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   // your code here
-// const form = document.querySelector('#create-task-form');
-//   form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const todo = document.getElementById('new-task-description').value;
-//   buildToDo(todo);
-//   form.reset();
-// });
-// });
-
-
-
-// const buildToDo = (todo) => {
-//   const li = document.createElement('li');
-//   const btn = document.createElement('button');
-//   btn.textContent = 'x';
-//   btn.addEventListener('click', handleDelete);
-//   li.textContent = todo;
-//   li.appendChild(btn);
-//   console.log(li);
-//   document.querySelector('#tasks').appendChild(li);
-// };
-
-// const handleDelete = (e) => {
-//   e.target.parentNode.remove();
-// };
-
-
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector('#create-task-form');
   form.addEventListener('submit', handleFormSubmit);
